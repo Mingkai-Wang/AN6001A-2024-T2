309cace07daf3acec0f34d268e5aff533f4ebb2f
Mingkai-Wang <159579563+Mingkai-Wang@users.noreply.github.com>
2024-11-18 07:45:43 +0000

    1

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 1
 #don't forget type 1 after comment then syncomment
 
-from flask import Flask, 
+from flask import Flask
 from flask import render_template, request
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    return(render_template("index.html"))
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=1111)
 1#
