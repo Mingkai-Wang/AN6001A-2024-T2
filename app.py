c20da349a978aec80961a49843494ea4d5320bb7
Mingkai-Wang <159579563+Mingkai-Wang@users.noreply.github.com>
2024-11-18 07:37:35 +0000

    1

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 1
 #don't forget type 1 after comment then syncomment
 
-from flask import Flask, render_template, request
+from flask import Flask, 
+from flask import render_template, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "PUT"])
+@app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html")
 
